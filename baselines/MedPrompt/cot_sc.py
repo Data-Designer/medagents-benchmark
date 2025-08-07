376ec782c493efab2a2d8d5d2dedacf6ec5dd568
Daniel Shao <19307110036@fudan.edu.cn>
2025-08-07 02:28:19 +0000

    [cot-sc] fix cot-sc o3-mini

--- baselines/MedPrompt/cot_sc.py
+++ baselines/MedPrompt/cot_sc.py
@@ -255,19 +255,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    client_old = AzureOpenAI(
-        api_version=os.getenv("AZURE_API_VERSION"),
-        azure_endpoint=os.getenv("AZURE_ENDPOINT"),
-        api_key=os.getenv("AZURE_API_KEY"),
+    client_old = OpenAI(
+        base_url=os.getenv("OPENAI_ENDPOINT"),
+        api_key=os.getenv("OPENAI_API_KEY"),
     )
-    
-    if args.model_name in ["o3-mini", "o1-mini"]:
-        client = AzureOpenAI(
-            api_version=os.getenv("AZURE_API_VERSION_2"),
-            azure_endpoint=os.getenv("AZURE_ENDPOINT_2"),
-            api_key=os.getenv("AZURE_API_KEY_2"),
-        )
-    elif args.model_name in ["Qwen/QwQ-32B-Preview", "deepseek-ai/DeepSeek-R1", "deepseek-ai/DeepSeek-V3", "meta-llama/Llama-3.3-70B-Instruct-Turbo"]:
+    if args.model_name in ["Qwen/QwQ-32B-Preview", "deepseek-ai/DeepSeek-R1", "deepseek-ai/DeepSeek-V3", "meta-llama/Llama-3.3-70B-Instruct-Turbo"]:
         client = OpenAI(
             base_url="https://api.together.xyz/v1",
             api_key=os.getenv("TOGETHER_API_KEY"),
